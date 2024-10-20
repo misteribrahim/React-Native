4cd8c4f338fd0959de859cb15caae9e8aad274ce
misteribrahim <misteribrahimk@gmail.com>
2024-10-20 16:09:07 +0500

    view txt button

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>Ibrahim React Native developer </Text>
+      <Button title="press here"/>
+    </View>
+  );
+};
+export default App;
